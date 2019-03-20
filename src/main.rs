001af034499587b90ec32451996becd62b3823c2
mohanson <mohanson@outlook.com>
2019-03-20 16:37:43 +0800

    Logger added in common

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use log::info;
+use logger;
 use transaction_pool::{order::FIFO, verifier::SECP256K1Verifier, TransactionPool};
 
 fn main() {
+    logger::init(logger::Flag::Main);
     let order = FIFO::new();
     let verifier = SECP256K1Verifier::new();
     let _tx_pool = TransactionPool::new(order, verifier);
 
-    println!("hello world");
+    info!("hello world");
 }
