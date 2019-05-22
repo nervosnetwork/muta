c727b733340029f72d9280a57e07522f635eff44
Wenchao Hu <me@huwenchao.com>
2019-05-22 21:38:36 +0800

    feat: add performance test lua script (#244)

---
+++ devtools/lua/erc20_transfer.lua
@@ -0,0 +1,234 @@
+local pb = require('pb')
+local protoc = require('protoc')
+local hex = require('hex')
+local json = require('json')
+local http = require('socket.http')
+local serpent = require('serpent')
+local socket = require('socket')
+
+local consts = require("consts")
+math.randomseed(os.time())
+
+-- load schema from text
+assert(
+    protoc:load [[
+  syntax = "proto3";
+  enum Crypto {
+      DEFAULT = 0;
+      RESERVED = 1;
+  }
+  message Transaction {
+      string to = 1;
+      string nonce = 2;
+      uint64 quota = 3;
+      uint64 valid_until_block = 4;
+      bytes data = 5;
+      bytes value = 6;
+      uint32 chain_id = 7;
+      uint32 version = 8;
+      bytes to_v1 = 9;
+      bytes chain_id_v1 = 10;
+  }
+  message UnverifiedTransaction {
+      Transaction transaction = 1;
+      bytes signature = 2;
+      Crypto crypto = 3;
+  }
+]]
+)
+
+
+local to_addr = '0000000000000000000000000000000000000001'
+local from_addr = '0x7899EE7319601cbC2684709e0eC3A4807bb0Fd74'
+local privkey = '0x028590ad352d54387a9c8a0ecf7e180e68c4840c72f958fc5917657f506caa80'
+local bnb_transfer_data = hex.decode(string.sub(consts.bnb_transfer_code, 3))
+
+
+-- {{method1, params1}, {method2, params2}}
+jsonrpc_call = function(params_list)
+    local request_body = {}
+    for k, v in pairs(params_list) do
+        request_body[k] = {
+            id = k,
+            jsonrpc = '2.0',
+            method = v[1],
+            params = v[2],
+        }
+    end
+    local request_body = json.encode(request_body)
+    -- print('<--', request_body)
+    local response_body = {}
+    http.request {
+        url = string.format('%s://%s:%s/', wrk.schema, wrk.host, wrk.port),
+        method = 'POST',
+        headers = {
+            ['Content-Type'] = 'application/json',
+            ['Content-Length'] = #request_body
+        },
+        source = ltn12.source.string(request_body),
+        sink = ltn12.sink.table(response_body)
+    }
+    local result = response_body[1]
+    -- print('-->', result)
+    local result_table = json.decode(result)
+    local res = {}
+    for k, v in pairs(result_table) do
+        res[k] = v['result']
+    end
+    return res
+end
+
+deploy_erc20 = function ()
+    local code = consts.bnb_bin
+    -- Params of [400000000, "DOUZ", 6, "DOUZ"]
+    local data = "0000000000000000000000000000000000000000000000000000000017d784000000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000600000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000004444f555a000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004444f555a00000000000000000000000000000000000000000000000000000000"
+    local tx = {
+        to = "",
+        nonce = string.format('deploy_erc20-%s', math.random()),
+        quota = 5000000,
+        valid_until_block = valid_until_block,
+        value = hex.decode('0000000000000000000000000000000000000000000000000000000000000000'),
+        data = hex.decode(consts.bnb_bin..data),
+    }
+    -- print('tx: ', serpent.block(tx))
+    local bytes = pb.encode('Transaction', tx)
+    local txdata = hex.encode(bytes)
+    local res = jsonrpc_call({
+        {'sendUnsafeTransaction', {txdata, privkey}}
+    })
+    print('deploy_erc20 res:', serpent.block(res))
+    -- get contract addr
+    local tx_hash = res[1]['hash']
+    local res = jsonrpc_call({
+        {'getTransactionReceipt', {tx_hash}}
+    })
+    while (#res == 0) do
+        socket.sleep(0.5)
+        res = jsonrpc_call({
+            {'getTransactionReceipt', {tx_hash}}
+        })
+        print('wait until contract deployed')
+    end
+    -- print('erc20 receipt:', serpent.block(res))
+    local contract_addr = res[1]['contractAddress']
+    print('deployed erc20 contract @', contract_addr)
+    return contract_addr
+end
+
+get_block_number_and_balance = function(contract_addr)
+    -- get erc20 balance
+    local res = jsonrpc_call({
+        {
+            'blockNumber',
+            {},
+        },
+        {'call', {{
+            to=contract_addr,
+            data=consts.bnb_balance_01_code,
+            from=from_addr,
+        }}}
+    })
+    -- print('balance: ', serpent.block(res))
+    return {
+        block_number=res[1],
+        balance=res[2],
+    }
+end
+
+wrk.method = 'POST'
+wrk.headers['Content-Type'] = 'application/json'
+
+
+local counter = 1
+
+function setup(thread)
+    thread:set("id", counter)
+    counter = counter + 1
+    if contract_addr == nil then
+        latest_block = tonumber(jsonrpc_call({{"blockNumber", {}}})[1])
+        valid_until_block = latest_block + 9999
+        print('valid_until_block:', valid_until_block)
+        contract_addr = deploy_erc20()
+        init_state = get_block_number_and_balance(contract_addr)
+        print('contract_addr:', contract_addr, 'init_state', serpent.block(init_state))
+    end
+    thread:set('contract_addr', contract_addr)
+    thread:set('init_state', init_state)
+    thread:set('valid_until_block', valid_until_block)
+ end
+
+ function init(args)
+    balance = tonumber(init_state['balance'])
+ end
+
+request = function()
+    local nonce = string.format('%s-%d-%s', id, balance, contract_addr)
+    balance = balance + 1
+    -- print(string.format("thread: %d, nonce: %s", id, nonce))
+    local tx = {
+        nonce = nonce,
+        quota = 210000,
+        valid_until_block = valid_until_block,
+        value = hex.decode('0000000000000000000000000000000000000000000000000000000000000000'),
+        data = bnb_transfer_data,
+        version = 1,
+        chain_id_v1 = hex.decode('0000000000000000000000000000000000000000000000000000000000000001'),
+        to_v1 = hex.decode(string.sub(contract_addr, 3))
+    }
+    -- print(serpent.block(tx))
+
+    local bytes = pb.encode('Transaction', tx)
+
+    -- send unsafe tx
+    local txdata = hex.encode(bytes)
+    local body = {
+        id = 1,
+        jsonrpc = '2.0',
+        method = 'sendUnsafeTransaction',
+        params = {txdata, privkey}
+    }
+
+    body_str = json.encode(body)
+    -- print(body_str)
+    return wrk.format('POST', '/', nil, body_str)
+end
+
+
+response = function(status, header, body)
+    -- print(body)
+end
+
+
+done = function(summary, latency, requests)
+    -- print('------------------------------')
+    -- print(serpent.block(summary))
+    -- print(string.format("init state: %s", serpent.block(init_state)))
+    local final_state = get_block_number_and_balance(contract_addr)
+    -- print(string.format("final state: %s", serpent.block(final_state)))
+    -- duration as s
+    local duration = summary['duration'] / 1000000
+    print(
+        string.format(
+            [[
+------------------------------------------------------
+| state |              balance |         block height |
+| init  | %20s | %20s |
+| final | %20s | %20s |
+------------------------------------------------------
+duration                    : %.3fs
+average tx num in a block   : %.0f
+average block time          : %.3fs
+average tx num in a second  : %.0f
+   ]],
+            tonumber(init_state['balance']),
+            init_state['block_number'],
+            tonumber(final_state['balance']),
+            final_state['block_number'],
+            duration,
+            (tonumber(final_state['balance']) - tonumber(init_state['balance'])) /
+                (tonumber(final_state['block_number']) - tonumber(init_state['block_number'])),
+            duration / (tonumber(final_state['block_number']) - tonumber(init_state['block_number'])),
+            (tonumber(final_state['balance']) - tonumber(init_state['balance'])) / duration
+        )
+    )
+end
---
+++ devtools/lua/peer_count.lua
@@ -0,0 +1,3 @@
+wrk.method = "POST"
+wrk.headers["Content-Type"] = "application/json"
+wrk.body = '{"jsonrpc":"2.0","method":"peerCount","params":[],"id":1}'
