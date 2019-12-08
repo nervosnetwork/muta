f02b219ad787994488ea30f37238409f35ec9215
Jiayu Ye <yejiayu.fe@gmail.com>
2019-12-08 22:58:35 +0800

    feat(protocol/traits): Add traits of binding. (#47)

--- protocol/src/traits/mod.rs
+++ protocol/src/traits/mod.rs
@@ -1,15 +1,16 @@
 mod api;
+mod binding;
 mod consensus;
+mod executor;
 mod mempool;
 mod network;
-mod runtime;
 mod storage;
 
 pub use api::APIAdapter;
-pub use consensus::{Consensus, ConsensusAdapter, MessageTarget, NodeInfo};
+pub use consensus::{Consensus, ConsensusAdapter, CurrentConsensusStatus, MessageTarget, NodeInfo};
+pub use executor::ExecutorResp;
 pub use mempool::{MemPool, MemPoolAdapter, MixedTxHashes};
 pub use network::{Gossip, MessageCodec, MessageHandler, Priority, Rpc};
-pub use runtime::RuntimeExecResp;
 pub use storage::{Storage, StorageAdapter, StorageBatchModify, StorageCategory, StorageSchema};
 
 pub use creep::{Cloneable, Context};
