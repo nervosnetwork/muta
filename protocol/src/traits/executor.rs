c06fa645f2e16e17568d523f6c517732263a6475
Jiayu Ye <yejiayu.fe@gmail.com>
2019-12-09 23:44:39 +0800

    feat(core/binding): Implementation of service state. (#48)

--- protocol/src/traits/executor.rs
+++ protocol/src/traits/executor.rs
@@ -1,9 +1,41 @@
 use crate::types::{Bloom, MerkleRoot, Receipt};
 
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Clone)]
 pub struct ExecutorResp {
     pub receipts:        Vec<Receipt>,
     pub all_cycles_used: u64,
     pub logs_bloom:      Bloom,
     pub state_root:      MerkleRoot,
 }
+// #[derive(Debug, Clone)]
+// pub struct ExecutorParams {
+//     state_root:   MerkleRoot,
+//     epoch_id:     u64,
+//     cycels_limit: u64,
+// }
+//
+// pub trait Executor {
+//     fn exec(
+//         &mut self,
+//         params: &ExecutorParams,
+//         txs: &[SignedTransaction],
+//     ) -> ProtocolResult<ExecutorResp>;
+//
+//     fn read(
+//         &self,
+//         params: &ExecutorParams,
+//         caller: &Address,
+//         request: &TransactionRequest,
+//     ) -> ProtocolResult<String>;
+// }
+//
+// pub trait ExecutorAdapter {
+//     fn get_service_inst<Inst: Service>(&self, service_name: &str) ->
+// ProtocolResult<Inst>;
+//
+//     fn revert_state(&mut self) -> ProtocolResult<()>;
+//
+//     fn stash_state(&mut self) -> ProtocolResult<()>;
+//
+//     fn commit_state(&mut self) -> ProtocolResult<MerkleRoot>;
+// }
