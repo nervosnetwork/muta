c785929dea3846c5816523477bec7d2a026e9015
yejiayu <yejiayu.fe@gmail.com>
2019-12-18 20:07:22 +0800

    feat(core/binding-macro): Add #[service] to automatically implement the service trait.

--- core/binding-macro/src/lib.rs
+++ core/binding-macro/src/lib.rs
@@ -3,94 +3,215 @@ extern crate proc_macro;
 mod common;
 mod cycles;
 mod read_write;
+mod servive;
 
 use proc_macro::TokenStream;
 
 use crate::cycles::gen_cycles_code;
 use crate::read_write::verify_read_or_write;
+use crate::servive::gen_service_code;
 
-// `#[read]` marks a service method as readable.
-//
-// Methods marked with this macro will have:
-//  Methods with this macro allow access (readable) from outside (RPC or other
-// services).
-//
-// - Verification
-//  1. Is it a struct method marked with #[service]?
-//  2. Is visibility private?
-//  3. Does function generics constrain `fn f<Context: RequestContext>`?
-//  4. Parameter signature contains `&self and ctx:Context`?
-//  5. Is the return value `ProtocolResult <JsonValue>`?
-//
-// example:
-//
-// struct Service;
-// #[service]
-// impl Service {
-//     #[read]
-//     fn test_read_fn<Context: RequestContext>(
-//         &self,
-//         _ctx: Context,
-//     ) -> ProtocolResult<JsonValue> {
-//         Ok(JsonValue::Null)
-//     }
-// }
+#[rustfmt::skip]
+/// `#[read]` marks a service method as readable.
+///
+/// Methods marked with this macro will have:
+///  Methods with this macro allow access (readable) from outside (RPC or other services).
+///
+/// - Verification
+///  1. Is it a struct method marked with #[service]?
+///  2. Is visibility private?
+///  3. Does function generics constrain `fn f<Context: RequestContext>`?
+///  4. Parameter signature contains `&self and ctx:Context`?
+///  5. Is the return value `ProtocolResult <JsonValue>`?
+///
+/// # Example:
+///
+/// ```rust
+/// struct Service;
+/// #[service]
+/// impl Service {
+///     #[read]
+///     fn test_read_fn<Context: RequestContext>(
+///         &self,
+///         _ctx: Context,
+///     ) -> ProtocolResult<JsonValue> {
+///         Ok(JsonValue::Null)
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn read(_: TokenStream, item: TokenStream) -> TokenStream {
     verify_read_or_write(item, false)
 }
 
-// `#[write]` marks a service method as writable.
-//
-// Methods marked with this macro will have:
-// - Accessibility
-//  Methods with this macro allow access (writeable) from outside (RPC or other
-// services).
-//
-// - Verification
-//  1. Is it a struct method marked with #[service]?
-//  2. Is visibility private?
-//  3. Does function generics constrain `fn f<Context: RequestContext>`?
-//  4. Parameter signature contains `&mut self and ctx:Context`?
-//  5. Is the return value `ProtocolResult <JsonValue>`?
-//
-// example:
-//
-// struct Service;
-// #[service]
-// impl Service {
-//     #[write]
-//     fn test_write_fn<Context: RequestContext>(
-//         &mut self,
-//         _ctx: Context,
-//     ) -> ProtocolResult<JsonValue> {
-//         Ok(JsonValue::Null)
-//     }
-// }
+#[rustfmt::skip]
+/// `#[write]` marks a service method as writable.
+///
+/// Methods marked with this macro will have:
+/// - Accessibility
+///  Methods with this macro allow access (writeable) from outside (RPC or other services).
+///
+/// - Verification
+///  1. Is it a struct method marked with #[service]?
+///  2. Is visibility private?
+///  3. Does function generics constrain `fn f<Context: RequestContext>`?
+///  4. Parameter signature contains `&mut self and ctx:Context`?
+///  5. Is the return value `ProtocolResult <JsonValue>`?
+///
+/// # Example:
+///
+/// ```rust
+/// struct Service;
+/// #[service]
+/// impl Service {
+///     #[write]
+///     fn test_write_fn<Context: RequestContext>(
+///         &mut self,
+///         _ctx: Context,
+///     ) -> ProtocolResult<JsonValue> {
+///         Ok(JsonValue::Null)
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn write(_: TokenStream, item: TokenStream) -> TokenStream {
     verify_read_or_write(item, true)
 }
 
-// `# [cycles]` mark an `ImplFn` or `fn`, it will automatically generate code to
-// complete the cycle deduction,
-//
-// // Source Code
-// impl Tests {
-//     #[cycles(100)]
-//     fn test_cycles<Context: RequestContext>(&self, ctx: Context) ->
-// ProtocolResult<()> {         Ok(())
-//     }
-// }
-//
-// // Generated code.
-// impl Tests {
-//     fn test_cycles<Context: RequestContext>(&self, ctx: Context) ->
-// ProtocolResult<()> {         ctx.sub_cycles(100)?;
-//         Ok(())
-//     }
-// }
+#[rustfmt::skip]
+/// `# [cycles]` mark an `ImplFn` or `fn`, it will automatically generate code
+/// to complete the cycle deduction,
+///
+/// ```rust
+/// // Source Code
+/// impl Tests {
+///     #[cycles(100)]
+///     fn test_cycles<Context: RequestContext>(&self, ctx: Context) -> ProtocolResult<()> {
+///         Ok(())
+///     }
+/// }
+///
+/// // Generated code.
+/// impl Tests {
+///     fn test_cycles<Context: RequestContext>(&self, ctx: Context) -> ProtocolResult<()> {
+///         ctx.sub_cycles(100)?;
+///         Ok(())
+///     }
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn cycles(attr: TokenStream, item: TokenStream) -> TokenStream {
     gen_cycles_code(attr, item)
+}
+
+/// Marks a method so that it executes before the entire block executes.
+// TODO(@yejiayu): Verify the function signature.
+#[proc_macro_attribute]
+pub fn hook_after(_: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+/// Marks a method so that it executes after the entire block executes.
+// TODO(@yejiayu): Verify the function signature.
+#[proc_macro_attribute]
+pub fn hook_before(_: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
+
+#[rustfmt::skip]
+/// Marking a ImplItem for service, it will automatically trait
+/// `protocol::traits::Service`.
+///
+/// # Example
+///
+/// use serde::{Deserialize, Serialize};
+/// use protocol::traits::RequestContext;
+/// use protocol::ProtocolResult;
+///
+/// ```rust
+/// // Source code
+/// 
+/// // serde::Deserialize and serde::Serialize are required.
+/// #[derive(Serialize, Deserialize)]
+/// struct CreateKittyPayload {
+///     // fields
+/// }
+/// 
+/// // serde::Deserialize and serde::Serialize are required.
+/// #[derive(Serialize, Deserialize)]
+/// struct GetKittyPayload {
+///     // fields
+/// }
+/// 
+/// #[service]
+/// impl KittyService {
+///     #[hook_before]
+///     fn custom_hook_before(&mut self) -> ProtoResult<()> {
+///         // Do something
+///     }
+/// 
+///     #[hook_after]
+///     fn custom_hook_after(&mut self) -> ProtoResult<()> {
+///         // Do something
+///     }
+/// 
+///     #[read]
+///     fn get_kitty<Context: RequestContext>(
+///         &self,
+///         ctx: Context,
+///         payload: GetKittyPayload,
+///     ) -> ProtoResult<&str> {
+///         // Do something
+///     }
+/// 
+///     #[write]
+///     fn create_kitty<Context: RequestContext>(
+///         &mut self,
+///         ctx: Context,
+///         payload: CreateKittyPayload,
+///     ) -> ProtoResult<&str> {
+///         // Do something
+///     }
+/// }
+/// 
+/// // Generated code.
+/// impl Service for KittyService {
+///     fn hook_before_(&mut self) -> ProtocolResult<()> {
+///         self.custom_hook_before()
+///     }
+/// 
+///     fn hook_after(&mut self) -> ProtocolResult<()> {
+///         self.custom_hook_after()
+///     }
+/// 
+///     fn write<Context: RequestContext>(&mut self, ctx: Context) -> ProtocolResult<&str> {
+///         let method = ctx.get_service_method();
+/// 
+///         match ctx.get_service_method() {
+///             "create_kitty" => {
+///                 let payload: CreateKittyPayload = serde_json::from_str(ctx.get_payload())
+///                     .map_err(|e| core_binding::ServiceError::JsonParse(e))?;
+///                 self.create_kitty(ctx, payload)
+///             }
+///             _ => Err(core_binding::ServiceError::NotFoundMethod(method.to_owned()).into()),
+///         }
+///     }
+/// 
+///     fn read<Context: RequestContext>(&self, ctx: Context) -> ProtocolResult<&str> {
+///         let method = ctx.get_service_method();
+/// 
+///         match ctx.get_service_method() {
+///             "get_kitty" => {
+///                 let payload: GetKittyPayload = serde_json::from_str(ctx.get_payload())
+///                     .map_err(|e| core_binding::ServiceError::JsonParse(e))?;
+///                 self.get_kitty(ctx, payload)
+///             }
+///             _ => Err(core_binding::ServiceError::NotFoundMethod(method.to_owned()).into()),
+///         }
+///     }
+/// }
+/// ```
+#[proc_macro_attribute]
+pub fn service(attr: TokenStream, item: TokenStream) -> TokenStream {
+    gen_service_code(attr, item)
 }
---
+++ core/binding-macro/src/servive.rs
@@ -0,0 +1,199 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, FnArg, Ident, ImplItem, ImplItemMethod, ItemImpl, Type};
+
+enum ServiceMethod {
+    Read(ImplItemMethod),
+    Write(ImplItemMethod),
+}
+
+struct Hooks {
+    before: Option<Ident>,
+    after:  Option<Ident>,
+}
+
+struct MethodMeta {
+    method_ident:  Ident,
+    payload_ident: Ident,
+    readonly:      bool,
+}
+
+pub fn gen_service_code(_: TokenStream, item: TokenStream) -> TokenStream {
+    let impl_item = parse_macro_input!(item as ItemImpl);
+
+    let service_ident = get_service_ident(&impl_item);
+    let items = &impl_item.items;
+    let generics = &impl_item.generics;
+
+    let mut methods: Vec<ServiceMethod> = vec![];
+
+    for item in items {
+        if let ImplItem::Method(method) = item {
+            if let Some(service_method) = find_service_method(method) {
+                methods.push(service_method)
+            }
+        }
+    }
+
+    let hooks = extract_hooks(items);
+    let hook_before = &hooks.before;
+    let hook_before_body = match hook_before {
+        Some(hook_before) => quote! { self.#hook_before() },
+        None => quote! {Ok(())},
+    };
+    let hook_after = &hooks.after;
+    let hook_after_body = match hook_after {
+        Some(hook_after) => quote! { self.#hook_after() },
+        None => quote! {Ok(())},
+    };
+
+    let list_method_meta: Vec<MethodMeta> = methods.into_iter().map(extract_method_meta).collect();
+
+    let (list_read_name, list_read_ident, list_read_payload) =
+        split_list_for_metadata(&list_method_meta, true);
+    let (list_write_name, list_write_ident, list_write_payload) =
+        split_list_for_metadata(&list_method_meta, false);
+
+    TokenStream::from(quote! {
+        impl#generics protocol::traits::Service for #service_ident#generics {
+            fn hook_before_(&mut self) -> protocol::ProtocolResult<()> {
+                #hook_before_body
+            }
+
+            fn hook_after_(&mut self) -> protocol::ProtocolResult<()> {
+                #hook_after_body
+            }
+
+            fn read_<Context: protocol::traits::RequestContext>(&self, ctx: Context) -> protocol::ProtocolResult<String> {
+                let method = ctx.get_service_method();
+
+                match method {
+                    #(#list_read_name => {
+                        let payload: #list_read_payload = serde_json::from_str(ctx.get_payload())
+                                .map_err(|e| core_binding::ServiceError::JsonParse(e))?;
+                        self.#list_read_ident(ctx, payload)
+                    },)*
+                    _ => Err(core_binding::ServiceError::NotFoundMethod(method.to_owned()).into())
+                }
+            }
+
+            fn write_<Context: protocol::traits::RequestContext>(&mut self, ctx: Context) -> protocol::ProtocolResult<String> {
+                let method = ctx.get_service_method();
+
+                match method {
+                    #(#list_write_name => {
+                        let payload: #list_write_payload = serde_json::from_str(ctx.get_payload())
+                                .map_err(|e| core_binding::ServiceError::JsonParse(e))?;
+                        self.#list_write_ident(ctx, payload)
+                    },)*
+                    _ => Err(core_binding::ServiceError::NotFoundMethod(method.to_owned()).into())
+                }
+            }
+        }
+
+        #impl_item
+    })
+}
+
+fn split_list_for_metadata(
+    list: &[MethodMeta],
+    readonly: bool,
+) -> (Vec<String>, Vec<Ident>, Vec<Ident>) {
+    let mut methods = vec![];
+    let mut method_idents = vec![];
+    let mut payload_idents = vec![];
+
+    list.iter()
+        .filter(|meta| meta.readonly == readonly)
+        .for_each(|meta| {
+            methods.push(meta.method_ident.to_string());
+            method_idents.push(meta.method_ident.clone());
+            payload_idents.push(meta.payload_ident.clone());
+        });
+    (methods, method_idents, payload_idents)
+}
+
+fn get_service_ident(impl_item: &ItemImpl) -> Ident {
+    match &*impl_item.self_ty {
+        Type::Path(type_path) => type_path
+            .path
+            .get_ident()
+            .expect("The identity of the service was not found.")
+            .clone(),
+        _ => panic!("The identity of the service was not found."),
+    }
+}
+
+fn find_service_method(method: &ImplItemMethod) -> Option<ServiceMethod> {
+    let attrs = &method.attrs;
+
+    for attr in attrs {
+        for segment in &attr.path.segments {
+            if segment.ident == "read" {
+                return Some(ServiceMethod::Read(method.clone()));
+            } else if segment.ident == "write" {
+                return Some(ServiceMethod::Write(method.clone()));
+            }
+        }
+    }
+
+    None
+}
+
+fn extract_hooks(items: &[ImplItem]) -> Hooks {
+    let mut hooks = Hooks {
+        before: None,
+        after:  None,
+    };
+
+    for item in items {
+        if let ImplItem::Method(method) = item {
+            for attr in &method.attrs {
+                for segment in &attr.path.segments {
+                    if segment.ident == "hook_before" {
+                        hooks.before = Some(method.sig.ident.clone())
+                    } else if segment.ident == "hook_after" {
+                        hooks.after = Some(method.sig.ident.clone())
+                    }
+                }
+            }
+        }
+    }
+
+    hooks
+}
+
+fn extract_method_meta(method: ServiceMethod) -> MethodMeta {
+    let (impl_method, readonly) = match method {
+        ServiceMethod::Read(impl_method) => (impl_method, true),
+        ServiceMethod::Write(impl_method) => (impl_method, false),
+    };
+
+    // inputs[0] = &self or &mut self
+    // inputs[1] = RequestContext
+    // inputs[2] = MethodPayload: FromStr
+    if impl_method.sig.inputs.len() != 3 {
+        panic!(
+            "The correct signature of the service method is: fn
+(&self/&mut self, RequestContext, Payload)"
+        );
+    }
+
+    let payload_arg = &impl_method.sig.inputs[2];
+    let pat_type = match payload_arg {
+        FnArg::Typed(pat_type) => pat_type,
+        _ => unreachable!(),
+    };
+
+    let payload_ident = if let Type::Path(path) = &*pat_type.ty {
+        path.path.get_ident().expect("")
+    } else {
+        panic!("")
+    };
+
+    MethodMeta {
+        method_ident: impl_method.sig.ident,
+        payload_ident: payload_ident.clone(),
+        readonly,
+    }
+}
