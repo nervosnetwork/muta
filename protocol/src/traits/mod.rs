f8c039f7946b4f21d4e86208e5d7be356328a1d4
zhounan <33241113+mkxbl@users.noreply.github.com>
2019-12-20 20:41:42 +0800

    [ᚬframework] feat(core/binding): add service_sdk, chain_querier, request_context (#58)
    * feat(core/binding): finish alloc type and chain querier of sdk
    
    * feat(core/binding): add sdk test
    
    * feat(core/binding): add service sdk and request context
    
    * fix style

--- protocol/src/traits/mod.rs
+++ protocol/src/traits/mod.rs
@@ -8,7 +8,7 @@ mod storage;
 
 pub use api::APIAdapter;
 pub use binding::{
-    AdmissionControl, ChainDB, RequestContext, Service, ServiceSDK, ServiceState, StoreArray,
+    AdmissionControl, ChainQuerier, RequestContext, Service, ServiceSDK, ServiceState, StoreArray,
     StoreBool, StoreMap, StoreString, StoreUint64,
 };
 pub use consensus::{Consensus, ConsensusAdapter, CurrentConsensusStatus, MessageTarget, NodeInfo};
