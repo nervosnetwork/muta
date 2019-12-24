e709474430dc2776b3d4ee506bace7ddb2bc9e51
Jiayu Ye <yejiayu.fe@gmail.com>
2019-12-24 18:28:41 +0800

    [ᚬframework] feat(core/binding-macro): Add #[init] attribute. (#64)
    * feat(core/binding-macro): Add #[init] attribute.
    
    * fix typo
    
    * fix binding trait

--- core/binding-macro/src/servive.rs
+++ core/binding-macro/src/servive.rs
@@ -2,6 +2,12 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, FnArg, Ident, ImplItem, ImplItemMethod, ItemImpl, Type};
 
+const INIT_ATTRIBUTE: &str = "init";
+const READ_ATTRIBUTE: &str = "read";
+const WRITE_ATTRIBUTE: &str = "write";
+const HOOK_BEFORE_ATTRIBUTE: &str = "hook_before";
+const HOOK_AFTER_ATTRIBUTE: &str = "hook_after";
+
 enum ServiceMethod {
     Read(ImplItemMethod),
     Write(ImplItemMethod),
@@ -16,6 +22,10 @@ struct MethodMeta {
     method_ident:  Ident,
     payload_ident: Ident,
     readonly:      bool,
+}
+
+struct InitMethod {
+    ident: Ident,
 }
 
 pub fn gen_service_code(_: TokenStream, item: TokenStream) -> TokenStream {
@@ -47,6 +57,8 @@ pub fn gen_service_code(_: TokenStream, item: TokenStream) -> TokenStream {
         None => quote! {Ok(())},
     };
 
+    let init_method_ident = extract_init_metod(items).ident;
+
     let list_method_meta: Vec<MethodMeta> = methods.into_iter().map(extract_method_meta).collect();
 
     let (list_read_name, list_read_ident, list_read_payload) =
@@ -55,7 +67,11 @@ pub fn gen_service_code(_: TokenStream, item: TokenStream) -> TokenStream {
         split_list_for_metadata(&list_method_meta, false);
 
     TokenStream::from(quote! {
-        impl #impl_generics protocol::traits::Service for #service_ident #ty_generics #where_clause {
+        impl #impl_generics protocol::traits::Service#ty_generics for #service_ident #ty_generics #where_clause {
+            fn init_(sdk: SDK) -> protocol::ProtocolResult<Self> {
+                Self::#init_method_ident(sdk)
+            }
+
             fn hook_before_(&mut self) -> protocol::ProtocolResult<()> {
                 #hook_before_body
             }
@@ -115,11 +131,7 @@ fn split_list_for_metadata(
 
 fn get_service_ident(impl_item: &ItemImpl) -> Ident {
     match &*impl_item.self_ty {
-        Type::Path(type_path) => type_path
-            .path
-            .get_ident()
-            .expect("The identity of the service was not found.")
-            .clone(),
+        Type::Path(type_path) => type_path.path.segments[0].ident.clone(),
         _ => panic!("The identity of the service was not found."),
     }
 }
@@ -129,9 +141,9 @@ fn find_service_method(method: &ImplItemMethod) -> Option<ServiceMethod> {
 
     for attr in attrs {
         for segment in &attr.path.segments {
-            if segment.ident == "read" {
+            if segment.ident == READ_ATTRIBUTE {
                 return Some(ServiceMethod::Read(method.clone()));
-            } else if segment.ident == "write" {
+            } else if segment.ident == WRITE_ATTRIBUTE {
                 return Some(ServiceMethod::Write(method.clone()));
             }
         }
@@ -140,8 +152,70 @@ fn find_service_method(method: &ImplItemMethod) -> Option<ServiceMethod> {
     None
 }
 
+fn extract_init_metod(items: &[ImplItem]) -> InitMethod {
+    let methods: Vec<ImplItemMethod> = find_list_for_item_method(items);
+
+    let mut syn_init_method: Option<ImplItemMethod> = None;
+
+    for method in methods {
+        let is_init_method = method.attrs.iter().any(|attr| {
+            attr.path
+                .segments
+                .iter()
+                .any(|segment| segment.ident == INIT_ATTRIBUTE)
+        });
+        if is_init_method && syn_init_method.is_none() {
+            syn_init_method = Some(method)
+        } else if is_init_method & syn_init_method.is_some() {
+            panic!("The init attribute can onlu have one")
+        }
+    }
+
+    let init_method = syn_init_method.expect("The init attribute must be added to a method");
+
+    InitMethod {
+        ident: init_method.sig.ident,
+    }
+}
+
 fn extract_hooks(items: &[ImplItem]) -> Hooks {
-    let methods: Vec<ImplItemMethod> = items
+    let methods: Vec<ImplItemMethod> = find_list_for_item_method(items);
+
+    let mut hooks = Hooks {
+        before: None,
+        after:  None,
+    };
+
+    let mut before_count = 0;
+    let mut after_count = 0;
+
+    for method in methods {
+        for attr in &method.attrs {
+            for segment in &attr.path.segments {
+                if segment.ident == HOOK_BEFORE_ATTRIBUTE {
+                    if before_count == 0 {
+                        hooks.before = Some(method.sig.ident.clone());
+                        before_count = 1;
+                    } else {
+                        panic!("The before hook can only have one")
+                    }
+                } else if segment.ident == HOOK_AFTER_ATTRIBUTE {
+                    if after_count == 0 {
+                        hooks.after = Some(method.sig.ident.clone());
+                        after_count = 1;
+                    } else {
+                        panic!("The after hook can only have one")
+                    }
+                }
+            }
+        }
+    }
+
+    hooks
+}
+
+fn find_list_for_item_method(items: &[ImplItem]) -> Vec<ImplItemMethod> {
+    items
         .iter()
         .filter(|item| {
             if let ImplItem::Method(_) = item {
@@ -157,39 +231,7 @@ fn extract_hooks(items: &[ImplItem]) -> Hooks {
                 unreachable!()
             }
         })
-        .collect();
-
-    let mut hooks = Hooks {
-        before: None,
-        after:  None,
-    };
-
-    let mut before_count = 0;
-    let mut after_count = 0;
-
-    for method in methods {
-        for attr in &method.attrs {
-            for segment in &attr.path.segments {
-                if segment.ident == "hook_before" {
-                    if before_count == 0 {
-                        hooks.before = Some(method.sig.ident.clone());
-                        before_count = 1;
-                    } else {
-                        panic!("The before hook can only have one")
-                    }
-                } else if segment.ident == "hook_after" {
-                    if after_count == 0 {
-                        hooks.after = Some(method.sig.ident.clone());
-                        after_count = 1;
-                    } else {
-                        panic!("The after hook can only have one")
-                    }
-                }
-            }
-        }
-    }
-
-    hooks
+        .collect()
 }
 
 fn extract_method_meta(method: ServiceMethod) -> MethodMeta {
