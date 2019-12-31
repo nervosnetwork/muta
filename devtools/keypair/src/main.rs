c4a379fe36f21b27cdd83a8289c75e855364e755
Wenchao Hu <huwenchao@cryptape.com>
2019-12-31 18:03:56 +0800

    [ᚬmaster] feat: add bls support for keypair (#76)
    * feat: add bls support for keypair
    
    * chore: add new line for cargo.toml
    
    * chore: format
    
    * feat: generate common_ref randomly
    
    * feat: add clap yml

--- devtools/keypair/src/main.rs
+++ devtools/keypair/src/main.rs
@@ -1,47 +1,73 @@
-use rand::{rngs::OsRng, thread_rng, RngCore};
+#[macro_use]
+extern crate clap;
+
+use std::convert::TryFrom;
+use std::default::Default;
+
+use clap::App;
+use rand::distributions::Alphanumeric;
+use rand::Rng;
+use rand::{rngs::OsRng, RngCore};
+use serde::Serialize;
 use tentacle_secio::SecioKeyPair;
 
-use common_crypto::{BlsPrivateKey, PrivateKey, PublicKey, ToBlsPublicKey};
+use common_crypto::{BlsPrivateKey, PublicKey, ToBlsPublicKey};
 use protocol::types::{Address, Hash};
 use protocol::BytesMut;
 
+#[derive(Default, Serialize, Debug)]
+struct Keypair {
+    pub index:          u32,
+    pub private_key:    String,
+    pub public_key:     String,
+    pub address:        String,
+    pub bls_public_key: String,
+}
+
+#[derive(Default, Serialize, Debug)]
+struct Output {
+    pub common_ref: String,
+    pub keypairs:   Vec<Keypair>,
+}
+
 pub fn main() {
-    let mut seed = [0u8; 32];
-    OsRng.fill_bytes(&mut seed);
+    let yml = load_yaml!("keypair.yml");
+    let m = App::from(yml).get_matches();
+    let number = value_t!(m, "number", u32).unwrap();
+    let common_ref = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(10)
+        .collect::<String>();
 
-    let seckey = Hash::digest(BytesMut::from(seed.as_ref()).freeze());
-    let keypair = SecioKeyPair::secp256k1_raw_key(seckey.as_bytes()).expect("secp256k1 keypair");
-    let pubkey = keypair.to_public_key().inner();
-    let user_addr = Address::from_pubkey_bytes(pubkey.into()).expect("user addr");
+    let mut output = Output {
+        common_ref: common_ref.clone(),
+        keypairs:   vec![],
+    };
 
-    println!("seckey hex: {:?}", seckey.as_hex());
-    println!(
-        "pubkey hex: {:?}",
-        hex::encode(keypair.to_public_key().inner())
-    );
-    println!("user addr hex: {}", user_addr.as_hex());
-    println!("================================================================");
+    for i in 0..number {
+        let mut k = Keypair::default();
 
-    let n: usize = ::std::env::args()
-        .last()
-        .unwrap()
-        .parse()
-        .expect("argument error");
-    let common_ref = "muta";
-    println!("common ref: {:?}", hex::encode(common_ref.as_bytes()));
+        let mut seed = [0u8; 32];
+        OsRng.fill_bytes(&mut seed);
 
-    for i in 0..n {
-        let priv_key = BlsPrivateKey::generate(&mut thread_rng());
-        let pub_key = priv_key.pub_key(&common_ref.into());
-        println!(
-            "bls private key {}: {:?}",
-            i + 1,
-            hex::encode(priv_key.to_bytes())
-        );
-        println!(
-            "bls public key {}: {:?}",
-            i + 1,
-            hex::encode(pub_key.to_bytes())
-        );
+        let seckey = Hash::digest(BytesMut::from(seed.as_ref()).freeze());
+        let keypair =
+            SecioKeyPair::secp256k1_raw_key(seckey.as_bytes()).expect("secp256k1 keypair");
+        let pubkey = keypair.to_public_key().inner();
+        let user_addr = Address::from_pubkey_bytes(pubkey.into()).expect("user addr");
+
+        k.private_key = seckey.as_hex();
+        k.public_key = hex::encode(keypair.to_public_key().inner());
+        k.address = user_addr.as_hex();
+
+        let priv_key =
+            BlsPrivateKey::try_from([&[0u8; 16], seckey.as_bytes().as_ref()].concat().as_ref())
+                .unwrap();
+        let pub_key = priv_key.pub_key(&common_ref.as_str().into());
+        k.bls_public_key = hex::encode(pub_key.to_bytes());
+        k.index = i + 1;
+        output.keypairs.push(k);
     }
+    let output_str = serde_json::to_string_pretty(&output).unwrap();
+    println!("{}", output_str);
 }
