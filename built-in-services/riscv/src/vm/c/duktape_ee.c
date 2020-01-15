7b20f6676c7cace5d7420c4b45e0b0522902d502
zero.qn <zero.qn@outlook.com>
2020-01-15 19:46:10 +0800

    chore(duktape_ee): format code

--- built-in-services/riscv/src/vm/c/duktape_ee.c
+++ built-in-services/riscv/src/vm/c/duktape_ee.c
@@ -10,38 +10,38 @@
 #define EE_ERR_COMPILE_CODE 3
 
 int main(int argc, char *argv[]) {
-    // Arguments should be exactly 'main' and 'js code'
-    if (2 != argc) {
-        return EE_ERR_ARGC_NUM;
-    }
+  // Arguments should be exactly 'main' and 'js code'
+  if (2 != argc) {
+    return EE_ERR_ARGC_NUM;
+  }
 
-    duk_context *ctx = duk_create_heap_default();
-    if (!ctx) {
-        return EE_ERR_INIT_CTX;
-    }
+  duk_context *ctx = duk_create_heap_default();
+  if (!ctx) {
+    return EE_ERR_INIT_CTX;
+  }
 
-    pvm_init(ctx);
+  pvm_init(ctx);
 
-    // Compile code
-    char *code = argv[1];
-    duk_push_string(ctx, "main");
-    if (0 != duk_pcompile_string(ctx, DUK_COMPILE_FUNCTION, code)) {
-        pvm_debug(duk_get_string(ctx, -1));
-        return EE_ERR_COMPILE_CODE;
-    }
+  // Compile code
+  char *code = argv[1];
+  duk_push_string(ctx, "main");
+  if (0 != duk_pcompile_string(ctx, DUK_COMPILE_FUNCTION, code)) {
+    pvm_debug(duk_get_string(ctx, -1));
+    return EE_ERR_COMPILE_CODE;
+  }
 
-    // Call our funtion
-    duk_int_t rc = duk_pcall(ctx, 0);
-    if (DUK_EXEC_SUCCESS == rc) {
-        const char *ret = duk_get_string(ctx, -1);
-        pvm_ret((uint8_t *)ret, strlen(ret));
-    } else {
-        const char *err_msg = duk_safe_to_string(ctx, -1);
-        pvm_ret((uint8_t *)err_msg, strlen(err_msg));
-    }
+  // Call our funtion
+  duk_int_t rc = duk_pcall(ctx, 0);
+  if (DUK_EXEC_SUCCESS == rc) {
+    const char *ret = duk_get_string(ctx, -1);
+    pvm_ret((uint8_t *)ret, strlen(ret));
+  } else {
+    const char *err_msg = duk_safe_to_string(ctx, -1);
+    pvm_ret((uint8_t *)err_msg, strlen(err_msg));
+  }
 
-    duk_pop(ctx);
-    duk_destroy_heap(ctx);
+  duk_pop(ctx);
+  duk_destroy_heap(ctx);
 
-    return rc;
+  return rc;
 }
--- built-in-services/riscv/src/vm/c/duktape_ee_helper.h
+++ built-in-services/riscv/src/vm/c/duktape_ee_helper.h
@@ -27,99 +27,102 @@ static void push_checked_integer(duk_context *ctx, uint64_t v) {
   }
 }
 
-
 // ####### PVM #######
 
 static duk_ret_t duk_pvm_debug(duk_context *ctx) {
-    duk_push_string(ctx, " ");
-    duk_insert(ctx, 0);
-    duk_join(ctx, duk_get_top(ctx) - 1);
-    pvm_debug(duk_safe_to_string(ctx, -1));
+  duk_push_string(ctx, " ");
+  duk_insert(ctx, 0);
+  duk_join(ctx, duk_get_top(ctx) - 1);
+  pvm_debug(duk_safe_to_string(ctx, -1));
 
-    return 0;
+  return 0;
 }
 
 static duk_ret_t duk_pvm_load_json_args(duk_context *ctx) {
-    duk_push_dynamic_buffer(ctx, 1024);
+  duk_push_dynamic_buffer(ctx, 1024);
 
-    void *args = duk_get_buffer(ctx, -1, NULL);
-    pvm_load_args(args, NULL);
+  void *args = duk_get_buffer(ctx, -1, NULL);
+  pvm_load_args(args, NULL);
 
-    duk_buffer_to_string(ctx, -1);
-    const char *json = duk_get_string(ctx, -1);
-    duk_pop(ctx);
+  duk_buffer_to_string(ctx, -1);
+  const char *json = duk_get_string(ctx, -1);
+  duk_pop(ctx);
 
-    duk_push_object(ctx);
-    duk_push_string(ctx, json);
-    duk_json_decode(ctx, -1);
+  duk_push_object(ctx);
+  duk_push_string(ctx, json);
+  duk_json_decode(ctx, -1);
 
-    duk_put_global_string(ctx, "ARGS");
+  duk_put_global_string(ctx, "ARGS");
 
-    return 0;
+  return 0;
 }
 
-static duk_ret_t duk_pvm_cycle_limit(duk_context* ctx) {
-    uint64_t cycle_limit;
+static duk_ret_t duk_pvm_cycle_limit(duk_context *ctx) {
+  uint64_t cycle_limit;
 
-    pvm_cycle_limit(&cycle_limit);
-    push_checked_integer(ctx, cycle_limit);
+  pvm_cycle_limit(&cycle_limit);
+  push_checked_integer(ctx, cycle_limit);
 
-    return 1;
+  return 1;
 }
 
 static duk_ret_t duk_pvm_get_storage(duk_context *ctx) {
-    if (!duk_is_string(ctx, -1)) {
-        duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR, "Invalid arguments, key should be string");
-        return duk_throw(ctx);
-    }
+  if (!duk_is_string(ctx, -1)) {
+    duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR,
+                          "Invalid arguments, key should be string");
+    return duk_throw(ctx);
+  }
 
-    const char *key = duk_get_string(ctx, -1);
-    duk_pop(ctx);
+  const char *key = duk_get_string(ctx, -1);
+  duk_pop(ctx);
 
-    duk_push_dynamic_buffer(ctx, 1024);
-    void *val = duk_get_buffer(ctx, -1, NULL);
+  duk_push_dynamic_buffer(ctx, 1024);
+  void *val = duk_get_buffer(ctx, -1, NULL);
 
-    pvm_get_storage((uint8_t *)key, strlen(key), val, NULL);
+  pvm_get_storage((uint8_t *)key, strlen(key), val, NULL);
 
-    duk_buffer_to_string(ctx, -1);
-    return 1;
+  duk_buffer_to_string(ctx, -1);
+  return 1;
 }
 
 static duk_ret_t duk_pvm_set_storage(duk_context *ctx) {
-    if (!duk_is_string(ctx, -1) || !duk_is_string(ctx, -2)) {
-        duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR, "Invalid arguments, should be string");
-        return duk_throw(ctx);
-    }
+  if (!duk_is_string(ctx, -1) || !duk_is_string(ctx, -2)) {
+    duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR,
+                          "Invalid arguments, should be string");
+    return duk_throw(ctx);
+  }
 
-    const char *key = duk_get_string(ctx, -2);
-    const char *val = duk_get_string(ctx, -1);
-    duk_pop_n(ctx, 2);
+  const char *key = duk_get_string(ctx, -2);
+  const char *val = duk_get_string(ctx, -1);
+  duk_pop_n(ctx, 2);
 
-    pvm_set_storage((uint8_t *)key, strlen(key), (uint8_t *)val, strlen(val));
+  pvm_set_storage((uint8_t *)key, strlen(key), (uint8_t *)val, strlen(val));
 
-    return 0;
+  return 0;
 }
 
 static duk_ret_t duk_pvm_contract_call(duk_context *ctx) {
-    if (!duk_is_string(ctx, -1) || !duk_is_string(ctx, -2)) {
-        duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR, "Invalid arguments, should be string");
-        return duk_throw(ctx);
-    }
+  if (!duk_is_string(ctx, -1) || !duk_is_string(ctx, -2)) {
+    duk_push_error_object(ctx, DUK_ERR_EVAL_ERROR,
+                          "Invalid arguments, should be string");
+    return duk_throw(ctx);
+  }
 
-    const char *addr = duk_get_string(ctx, -2);
-    const char *call_args = duk_get_string(ctx, -1);
-    duk_pop_n(ctx, 2);
+  const char *addr = duk_get_string(ctx, -2);
+  const char *call_args = duk_get_string(ctx, -1);
+  duk_pop_n(ctx, 2);
 
-    duk_push_dynamic_buffer(ctx, 1024);
-    void *ret = duk_get_buffer(ctx, -1, NULL);
+  duk_push_dynamic_buffer(ctx, 1024);
+  void *ret = duk_get_buffer(ctx, -1, NULL);
 
-    int ret_code = 0;
-    if (ret_code != pvm_contract_call((uint8_t *)addr, (uint8_t *)call_args, strlen(call_args), ret, NULL)) {
-        return ret_code;
-    }
+  int ret_code = 0;
+  if (ret_code != pvm_contract_call((uint8_t *)addr, (uint8_t *)call_args,
+                                    strlen(call_args), ret, NULL)) {
+    return ret_code;
+  }
 
-    duk_buffer_to_string(ctx, -1);
-    return 1;
+  duk_buffer_to_string(ctx, -1);
+  return 1;
 }
 
 void pvm_init(duk_context *ctx) {
