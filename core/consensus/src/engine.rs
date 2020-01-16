3e7f0923e364bf411276f5e55bf544a5526d8400
Eason <kaoimin@qq.com>
2020-01-16 17:03:34 +0800

    delete some annotations

--- core/consensus/src/engine.rs
+++ core/consensus/src/engine.rs
@@ -468,7 +468,6 @@ impl<Adapter: ConsensusAdapter + 'static> ConsensusEngine<Adapter> {
         Ok(())
     }
 
-    /// **TODO:** parallelism
     /// After get the signed transactions:
     /// 1. Execute the signed transactions.
     /// 2. Save the signed transactions.
@@ -499,42 +498,6 @@ impl<Adapter: ConsensusAdapter + 'static> ConsensusEngine<Adapter> {
         log::info!("update_status update_after_commit end {:?}", len);
         self.save_wal().await
     }
-
-    // pub async fn save_proof(&self, ctx: Context, proof: Proof) ->
-    // ProtocolResult<()> {     self.adapter.save_proof(ctx, proof).await
-    // }
-
-    // pub fn get_current_interval(&self) -> u64 {
-    //     let current_consensus_status = self.status_agent.to_inner();
-    //     current_consensus_status.consensus_interval
-    // }
-
-    // pub fn get_current_authority_list(&self) -> Vec<Node> {
-    //     let current_consensus_status = self.status_agent.to_inner();
-    //     covert_to_overlord_authority(&current_consensus_status.validators)
-    // }
-
-    // pub fn get_current_state_root(&self, epoch_id: u64) ->
-    // ProtocolResult<Option<MerkleRoot>> {     let current_consensus_status =
-    // self.status_agent.to_inner();     if epoch_id ==
-    // current_consensus_status.exec_epoch_id {         let state_root =
-    // current_consensus_status             .state_root
-    //             .last()
-    //             .ok_or_else(||
-    // ConsensusError::StatusErr(StatusCacheField::StateRoot))?;         return
-    // Ok(Some(state_root.clone()));     }
-    //     Ok(None)
-    // }
-
-    // pub fn check_state_root(&self, state_root: &MerkleRoot) -> bool {
-    //     let current_consensus_status = self.status_agent.to_inner();
-    //     current_consensus_status.state_root.contains(state_root)
-    // }
-
-    // pub fn get_current_prev_hash(&self) -> Hash {
-    //     let current_consensus_status = self.status_agent.to_inner();
-    //     current_consensus_status.prev_hash
-    // }
 
     async fn save_wal(&self) -> ProtocolResult<()> {
         let mut info = self.status_agent.to_inner();
