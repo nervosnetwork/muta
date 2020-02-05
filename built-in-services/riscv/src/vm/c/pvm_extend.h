d39f96e34352db6281991218b50589b910504d7f
zero.qn <zero.qn@outlook.com>
2020-02-05 13:17:23 +0800

    doc(riscv): document pvm_extend.h

--- built-in-services/riscv/src/vm/c/pvm_extend.h
+++ built-in-services/riscv/src/vm/c/pvm_extend.h
@@ -3,6 +3,17 @@
 
 #include "pvm.h"
 
+/*
+ * Function pvm_hex2bin decode given hex string to binary data.
+ *
+ * Params:
+ *   s[in]: string to decode
+ *
+ *   buf[out]: pointer to buffer for decoded data to write
+ *
+ * Return:
+ *   n: size of decoded data
+ */
 int pvm_hex2bin(char *s, char *buf) {
   int i, n = 0;
   for (i = 0; s[i]; i += 2) {
@@ -20,6 +31,18 @@ int pvm_hex2bin(char *s, char *buf) {
   return n;
 }
 
+/*
+ * Function pvm_bin2hex encode given data to hex string
+ *
+ * Params:
+ *   bin[in]: pointer to data to encode
+ *   len[in]: size of data
+ *
+ *   out[out]: pointer to buffer for encoded string to write
+ *
+ * Return:
+ *   0(success)
+ */
 int pvm_bin2hex(uint8_t *bin, uint8_t len, char *out) {
   uint8_t i;
   for (i = 0; i < len; i++) {
@@ -30,9 +53,29 @@ int pvm_bin2hex(uint8_t *bin, uint8_t len, char *out) {
   return 0;
 }
 
+/*
+ * Function pvm_ret_str is a wrapper function to pvm_ret to easily return
+ * string.
+ *
+ * Params:
+ *   s[in]: string to return
+ *
+ * Return:
+ *   0(success)
+ */
 int pvm_ret_str(const char *s) {
   uint8_t *buffer = (uint8_t *)s;
   return pvm_ret(&buffer[0], strlen(buffer));
 }
 
+/*
+ * Function pvm_ret_u64 is a wrapper function to pvm_ret to easily return
+ * uint64_t.
+ *
+ * Params:
+ *   n[in]: given number to return
+ *
+ * Return:
+ *   0(success)
+ */
 int pvm_ret_u64(uint64_t n) { return pvm_ret((uint8_t *)&n, 8); }
