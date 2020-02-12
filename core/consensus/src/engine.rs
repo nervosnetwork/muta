b258f4d64a492672ae0b8e1f2c1f2c0b1c1ebb4b
Eason <kaoimin@qq.com>
2020-02-12 15:05:24 +0800

    fix: sync txs context no session id

--- core/consensus/src/engine.rs
+++ core/consensus/src/engine.rs
@@ -119,16 +119,16 @@ impl<Adapter: ConsensusAdapter + 'static> Engine<FixedPill> for ConsensusEngine<
             self.adapter
                 .check_txs(ctx.clone(), order_hashes.clone())
                 .await?;
+
+            tokio::spawn(async move {
+                if let Err(e) = sync_txs(ctx, adapter, sync_tx_hashes).await {
+                    error!("Consensus sync block error {}", e);
+                }
+            });
         }
 
         let inner = self.adapter.get_full_txs(ctx.clone(), order_hashes).await?;
         let adapter = Arc::clone(&self.adapter);
-
-        tokio::spawn(async move {
-            if let Err(e) = sync_txs(ctx, adapter, sync_tx_hashes).await {
-                error!("Consensus sync block error {}", e);
-            }
-        });
         self.adapter
             .save_wal_transactions(Context::new(), Hash::digest(hash.clone()), inner)
             .await?;
