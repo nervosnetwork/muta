dad3f99f7c694eea57b546c6b2169950c5692ea1
zhounan <33241113+mkxbl@users.noreply.github.com>
2020-02-14 12:33:25 +0800

    Revert "[ᚬdebug-muta] feat(service): Upgrade asset (#181)" (#182)
    This reverts commit 9051f6a4d7519307c8647f0d96eb83427e614339.

--- framework/src/executor/tests/service_call_service.rs
+++ framework/src/executor/tests/service_call_service.rs
@@ -74,7 +74,7 @@ fn test_service_call_service() {
     let txs = vec![stx];
     let executor_resp = executor.exec(&params, &txs).unwrap();
     let receipt = &executor_resp.receipts[0];
-    let event = &receipt.events[1];
+    let event = &receipt.events[0];
 
     assert_eq!(50_000, receipt.cycles_used);
     assert_eq!(
