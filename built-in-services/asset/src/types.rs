6ca3038eeba08fd64bbe85b0dce85fe69877ef0a
zhounan <33241113+mkxbl@users.noreply.github.com>
2020-02-14 12:33:25 +0800

    Revert "[ᚬdebug-muta] feat(service): Upgrade asset (#181)" (#182)
    This reverts commit 9051f6a4d7519307c8647f0d96eb83427e614339.

--- built-in-services/asset/src/types.rs
+++ built-in-services/asset/src/types.rs
@@ -1,5 +1,3 @@
-use std::collections::BTreeMap;
-
 use serde::{Deserialize, Serialize};
 
 use bytes::Bytes;
@@ -38,66 +36,15 @@ pub struct TransferPayload {
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct TransferEvent {
-    pub asset_id: Hash,
-    pub from:     Address,
-    pub to:       Address,
-    pub value:    u64,
-}
-
-pub type ApprovePayload = TransferPayload;
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct ApproveEvent {
-    pub asset_id: Hash,
-    pub grantor:  Address,
-    pub grantee:  Address,
-    pub value:    u64,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct TransferFromPayload {
-    pub asset_id:  Hash,
-    pub sender:    Address,
-    pub recipient: Address,
-    pub value:     u64,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct TransferFromEvent {
-    pub asset_id:  Hash,
-    pub caller:    Address,
-    pub sender:    Address,
-    pub recipient: Address,
-    pub value:     u64,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct GetBalancePayload {
     pub asset_id: Hash,
-    pub user:     Address,
 }
 
+/// Response
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct GetBalanceResponse {
     pub asset_id: Hash,
-    pub user:     Address,
     pub balance:  u64,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct GetAllowancePayload {
-    pub asset_id: Hash,
-    pub grantor:  Address,
-    pub grantee:  Address,
-}
-
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct GetAllowanceResponse {
-    pub asset_id: Hash,
-    pub grantor:  Address,
-    pub grantee:  Address,
-    pub value:    u64,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
@@ -107,16 +54,6 @@ pub struct Asset {
     pub symbol: String,
     pub supply: u64,
     pub issuer: Address,
-}
-
-pub struct AssetBalance {
-    pub value:     u64,
-    pub allowance: BTreeMap<Address, u64>,
-}
-
-struct AllowanceCodec {
-    pub addr:  Address,
-    pub total: u64,
 }
 
 impl rlp::Decodable for Asset {
@@ -143,64 +80,6 @@ impl rlp::Encodable for Asset {
 }
 
 impl FixedCodec for Asset {
-    fn encode_fixed(&self) -> ProtocolResult<Bytes> {
-        Ok(Bytes::from(rlp::encode(self)))
-    }
-
-    fn decode_fixed(bytes: Bytes) -> ProtocolResult<Self> {
-        Ok(rlp::decode(bytes.as_ref()).map_err(FixedCodecError::from)?)
-    }
-}
-
-impl rlp::Decodable for AllowanceCodec {
-    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
-        Ok(Self {
-            addr:  rlp.at(0)?.as_val()?,
-            total: rlp.at(1)?.as_val()?,
-        })
-    }
-}
-
-impl rlp::Encodable for AllowanceCodec {
-    fn rlp_append(&self, s: &mut rlp::RlpStream) {
-        s.begin_list(2).append(&self.addr).append(&self.total);
-    }
-}
-
-impl rlp::Decodable for AssetBalance {
-    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
-        let value = rlp.at(0)?.as_val()?;
-        let codec_list: Vec<AllowanceCodec> = rlp::decode_list(rlp.at(1)?.as_raw());
-        let mut allowance = BTreeMap::new();
-        for v in codec_list {
-            allowance.insert(v.addr, v.total);
-        }
-
-        Ok(AssetBalance { value, allowance })
-    }
-}
-
-impl rlp::Encodable for AssetBalance {
-    fn rlp_append(&self, s: &mut rlp::RlpStream) {
-        s.begin_list(2);
-        s.append(&self.value);
-
-        let mut codec_list = Vec::with_capacity(self.allowance.len());
-
-        for (address, allowance) in self.allowance.iter() {
-            let fixed_codec = AllowanceCodec {
-                addr:  address.clone(),
-                total: *allowance,
-            };
-
-            codec_list.push(fixed_codec);
-        }
-
-        s.append_list(&codec_list);
-    }
-}
-
-impl FixedCodec for AssetBalance {
     fn encode_fixed(&self) -> ProtocolResult<Bytes> {
         Ok(Bytes::from(rlp::encode(self)))
     }
