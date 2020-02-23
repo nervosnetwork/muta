1634530154787c22056d4d63e39d8b55e34b114b
Eason <kaoimin@qq.com>
2020-02-24 00:24:54 +0800

    fix: always send rich status

--- core/consensus/src/consensus.rs
+++ core/consensus/src/consensus.rs
@@ -100,22 +100,21 @@ impl<Adapter: ConsensusAdapter + 'static> OverlordConsensus<Adapter> {
         let overlord_handler = overlord.get_handler();
 
         let status = status_agent.to_inner();
-        if status.height == 1 {
-            overlord_handler
-                .send_msg(
-                    Context::new(),
-                    OverlordMsg::RichStatus(gen_overlord_status(
-                        status.height,
-                        status.consensus_interval,
-                        status.propose_ratio,
-                        status.prevote_ratio,
-                        status.precommit_ratio,
-                        status.brake_ratio,
-                        status.validators,
-                    )),
-                )
-                .unwrap();
-        }
+
+        overlord_handler
+            .send_msg(
+                Context::new(),
+                OverlordMsg::RichStatus(gen_overlord_status(
+                    status.height,
+                    status.consensus_interval,
+                    status.propose_ratio,
+                    status.prevote_ratio,
+                    status.precommit_ratio,
+                    status.brake_ratio,
+                    status.validators,
+                )),
+            )
+            .unwrap();
 
         Self {
             inner:   Arc::new(overlord),
@@ -168,7 +167,7 @@ pub fn gen_overlord_status(
             .map(|node| hex::encode(node.address.clone()))
             .collect::<Vec<_>>()
     );
-    
+
     Status {
         height,
         interval: Some(interval),
