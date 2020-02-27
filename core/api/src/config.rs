cbf65eeaa025f5489a3ff5d6610600a1b1aef846
Mohanson <mohanson@outlook.com>
2020-02-27 10:37:54 +0800

    chore(config): the effect of the maxconn field matches its name now (#206)

--- core/api/src/config.rs
+++ core/api/src/config.rs
@@ -11,9 +11,7 @@ pub struct GraphQLConfig {
     // By default http server uses number of available logical cpu as threads count.
     pub workers: usize,
 
-    // Sets the maximum per-worker number of concurrent connections.
-    // All socket listeners will stop accepting connections when this limit is reached for each
-    // worker. By default max connections is set to a 25k.
+    // Sets the maximum number of all concurrent connections.
     pub maxconn: usize,
 
     // Set the max payload size of graphql interface.
