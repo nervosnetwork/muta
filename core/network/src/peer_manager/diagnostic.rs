94b34e6c43dd6ae8ffdd90ae869cec2823528457
zero.qn <zero.qn@outlook.com>
2020-04-09 14:09:00 +0800

    feat(network): expose trust metric basic function to diagnostic

--- core/network/src/peer_manager/diagnostic.rs
+++ core/network/src/peer_manager/diagnostic.rs
@@ -1,9 +1,18 @@
-use super::Inner;
+use super::{Inner, TrustMetric, WORSE_TRUST_SCALAR_RATIO};
 
+use derive_more::Display;
 use protocol::types::Address;
 use tentacle::SessionId;
 
 use std::sync::Arc;
+
+pub type GoodEvents = usize;
+pub type BadEvents = usize;
+
+#[derive(Debug, Display)]
+#[display(fmt = "not found")]
+pub struct NotFound {}
+impl std::error::Error for NotFound {}
 
 #[derive(Clone)]
 pub struct Diagnostic(Arc<Inner>);
@@ -20,5 +29,32 @@ impl Diagnostic {
             Some(sid) if sid != SessionId::new(0) => Some(sid),
             _ => None,
         }
+    }
+
+    pub fn trust_metric_wrose_scalar_ratio(&self) -> usize {
+        WORSE_TRUST_SCALAR_RATIO
+    }
+
+    pub fn session_trust_score(&self, sid: SessionId) -> Option<u8> {
+        self.session_trust_metric(sid)
+            .map(|metric| metric.trust_score())
+    }
+
+    pub fn session_trust_events(&self, sid: SessionId) -> Option<(GoodEvents, BadEvents)> {
+        self.session_trust_metric(sid).map(|metric| metric.events())
+    }
+
+    pub fn session_trust_force_new_interval(&self, sid: SessionId) -> Result<(), NotFound> {
+        let trust_metric = self.session_trust_metric(sid).ok_or_else(|| NotFound {})?;
+
+        trust_metric.enter_new_interval();
+        Ok(())
+    }
+
+    fn session_trust_metric(&self, sid: SessionId) -> Option<TrustMetric> {
+        self.0
+            .session(sid)
+            .map(|sess| sess.peer.trust_metric())
+            .flatten()
     }
 }
