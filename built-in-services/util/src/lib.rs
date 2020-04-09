75273a9d19a1ebfc4c3f64537dd4e54ce93cd699
Ethan <fendouhyz@163.com>
2020-04-09 17:32:12 +0800

    fix pr review, add service err return, add test_verify

--- built-in-services/util/src/lib.rs
+++ built-in-services/util/src/lib.rs
@@ -1,23 +1,16 @@
-#[cfg(test)]
-mod tests;
-pub mod types;
-
-
-use std::collections::BTreeMap;
-
 use bytes::Bytes;
 use hasher::{Hasher, HasherKeccak};
 
-use binding_macro::{cycles, genesis, service};
-use common_crypto::{
-    Crypto, HashValue, PrivateKey, PublicKey, Secp256k1, Secp256k1PrivateKey, Secp256k1PublicKey,
-    Secp256k1Signature, Signature, ToPublicKey,
-};
-use protocol::traits::{ExecutorParams, ServiceResponse, ServiceSDK, StoreMap};
-use protocol::types::{Address, Hash, Hex, ServiceContext};
+use binding_macro::{cycles, service};
+use common_crypto::{Crypto, Secp256k1};
+use protocol::traits::{ExecutorParams, ServiceResponse, ServiceSDK};
+use protocol::types::{Hash, ServiceContext};
 
 use crate::types::{KeccakPayload, KeccakResponse, SigVerifyPayload, SigVerifyResponse};
 
+#[cfg(test)]
+mod tests;
+pub mod types;
 
 pub struct UtilService<SDK> {
     sdk: SDK,
@@ -25,7 +18,7 @@ pub struct UtilService<SDK> {
 
 #[service]
 impl<SDK: ServiceSDK> UtilService<SDK> {
-    pub fn new(mut sdk: SDK) -> Self {
+    pub fn new(sdk: SDK) -> Self {
         Self { sdk }
     }
 
@@ -37,23 +30,16 @@ impl<SDK: ServiceSDK> UtilService<SDK> {
         payload: KeccakPayload,
     ) -> ServiceResponse<KeccakResponse> {
         let keccak = HasherKeccak::new();
+        let data = hex::decode(payload.hex_str.as_string_trim0x());
+        if data.is_err() {
+            return ServiceResponse::<KeccakResponse>::from_error(107, "data not valid".to_owned());
+        }
 
-        let data = hex::decode(payload.hex_str.as_string_trim0x()).unwrap();
-        let hash_res = keccak.digest(data.as_slice());
-
-        return match Hash::from_bytes(Bytes::from(hash_res)) {
-            Ok(res) => {
-                let response = KeccakResponse {
-                    result: res,
-                };
-                ServiceResponse::<KeccakResponse>::from_succeed(response)
-            }
-
-            _ => ServiceResponse::<KeccakResponse>::from_error(
-                101,
-                "data not valid".to_owned(),
-            )
+        let hash_res = keccak.digest(data.unwrap().as_slice());
+        let response = KeccakResponse {
+            result: Hash::from_bytes(Bytes::from(hash_res)).unwrap(),
         };
+        ServiceResponse::<KeccakResponse>::from_succeed(response)
     }
 
     #[cycles(100_00)]
@@ -63,12 +49,31 @@ impl<SDK: ServiceSDK> UtilService<SDK> {
         ctx: ServiceContext,
         payload: SigVerifyPayload,
     ) -> ServiceResponse<SigVerifyResponse> {
-        let data_sig = hex::decode(payload.sig.as_string_trim0x()).unwrap();
-        let data_pk = hex::decode(payload.pub_key.as_string_trim0x()).unwrap();
+        let data_sig = hex::decode(payload.sig.as_string_trim0x());
+        if data_sig.is_err() {
+            return ServiceResponse::<SigVerifyResponse>::from_error(
+                108,
+                "signature not valid".to_owned(),
+            );
+        };
+
+        let data_pk = hex::decode(payload.pub_key.as_string_trim0x());
+        if data_pk.is_err() {
+            return ServiceResponse::<SigVerifyResponse>::from_error(
+                109,
+                "public key not valid".to_owned(),
+            );
+        };
+
         let data_hash = payload.hash.as_bytes();
 
         let response = SigVerifyResponse {
-            is_ok: Secp256k1::verify_signature(data_hash.as_ref(), data_sig.as_slice(),data_pk.as_slice()).is_ok()
+            is_ok: Secp256k1::verify_signature(
+                data_hash.as_ref(),
+                data_sig.unwrap().as_slice(),
+                data_pk.unwrap().as_slice(),
+            )
+            .is_ok(),
         };
 
         ServiceResponse::<SigVerifyResponse>::from_succeed(response)
