8676dc3ded2249815c605e3dd0fa3db13e181ada
zero.qn <zero.qn@outlook.com>
2020-05-11 18:14:23 +0800

    change(metric): replace super with crate

--- common/apm/src/metrics/network.rs
+++ common/apm/src/metrics/network.rs
@@ -1,4 +1,4 @@
-use super::{
+use crate::metrics::{
     auto_flush_from, exponential_buckets, make_auto_flush_static_metric, register_histogram_vec,
     register_int_counter_vec, register_int_gauge, register_int_gauge_vec, HistogramVec,
     IntCounterVec, IntGauge, IntGaugeVec,
