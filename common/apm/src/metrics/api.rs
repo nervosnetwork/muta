6ea21e3e0fe08898264f13938cf849c197531afa
zeroqn <zero.qn@outlook.com>
2020-05-12 10:43:14 +0800

    feat(apm): add api get_block metrics (#276)
    * change(apm): request time exponential bucket start from 1ms (0.001)
    
    * feat(apm): add metrics to api get_block
    
    * change(apm): replace super with crate

--- common/apm/src/metrics/api.rs
+++ common/apm/src/metrics/api.rs
@@ -1,4 +1,4 @@
-use super::{
+use crate::metrics::{
     auto_flush_from, exponential_buckets, make_auto_flush_static_metric, register_histogram_vec,
     register_int_counter_vec, HistogramVec, IntCounterVec,
 };
@@ -8,7 +8,7 @@ use lazy_static::lazy_static;
 make_auto_flush_static_metric! {
     pub label_enum RequestKind {
         send_transaction,
-        query,
+        get_block,
     }
 
     pub label_enum SendTransactionResult {
@@ -42,12 +42,11 @@ lazy_static! {
         &["type", "result"]
     )
     .expect("request result total");
-    // TODO: appropriate exponential buckets number
     pub static ref API_REQUEST_TIME_HISTOGRAM_VEC: HistogramVec = register_histogram_vec!(
         "muta_api_request_time_cost_seconds",
         "Request process time cost",
         &["type"],
-        exponential_buckets(1.0, 2.0, 10).expect("api req time expontial")
+        exponential_buckets(0.001, 2.0, 20).expect("api req time expontial")
     )
     .expect("request time cost");
 }
