6509cbec2f700238b2259943212e0968b58404ce
zeroqn <zero.qn@outlook.com>
2020-05-13 17:51:07 +0800

    feat(network): add trace header to network msg (#281)
    * feat(network): add trace header to network msg
    
    * change(network): replace trace with headers
    
    * feat(network): add span id to message header
    
    * fix(apm): tracing span compilation

--- core/network/src/message/mod.rs
+++ core/network/src/message/mod.rs
@@ -1,6 +1,8 @@
 pub mod serde;
 pub mod serde_multi;
 
+use common_apm::muta_apm::rustracing_jaeger::span::TraceId;
+
 use derive_more::Constructor;
 use futures::channel::mpsc::UnboundedSender;
 use prost::Message;
@@ -14,6 +16,8 @@ use crate::{
     event::PeerManagerEvent,
 };
 
+use std::{collections::HashMap, str::FromStr};
+
 #[derive(Constructor)]
 #[non_exhaustive]
 pub struct RawSessionMessage {
@@ -22,21 +26,65 @@ pub struct RawSessionMessage {
     pub(crate) msg: Bytes,
 }
 
+pub struct Headers(HashMap<String, Vec<u8>>);
+
+impl Default for Headers {
+    fn default() -> Self {
+        Headers(Default::default())
+    }
+}
+
+impl Headers {
+    pub fn set_trace_id(&mut self, id: TraceId) {
+        self.0
+            .insert("trace_id".to_owned(), id.to_string().into_bytes());
+    }
+
+    pub fn set_span_id(&mut self, id: u64) {
+        self.0
+            .insert("span_id".to_owned(), id.to_be_bytes().to_vec());
+    }
+}
+
 #[derive(Message)]
 pub struct NetworkMessage {
-    #[prost(string, tag = "1")]
+    #[prost(map = "string, bytes", tag = "1")]
+    pub headers: HashMap<String, Vec<u8>>,
+
+    #[prost(string, tag = "2")]
     pub url: String,
 
-    #[prost(bytes, tag = "2")]
+    #[prost(bytes, tag = "3")]
     pub content: Vec<u8>,
 }
 
 impl NetworkMessage {
-    pub fn new(endpoint: Endpoint, content: Bytes) -> Self {
+    pub fn new(endpoint: Endpoint, content: Bytes, headers: Headers) -> Self {
         NetworkMessage {
+            headers: headers.0,
             url:     endpoint.full_url().to_owned(),
             content: content.to_vec(),
         }
+    }
+
+    pub fn trace_id(&self) -> Option<TraceId> {
+        self.headers
+            .get("trace_id")
+            .map(|id| {
+                String::from_utf8(id.to_owned())
+                    .ok()
+                    .map(|s| TraceId::from_str(&s).ok())
+                    .flatten()
+            })
+            .flatten()
+    }
+
+    pub fn span_id(&self) -> Option<u64> {
+        self.headers.get("span_id").map(|id| {
+            let mut buf = [0u8; 8];
+            buf.copy_from_slice(&id[..8]);
+            u64::from_be_bytes(buf)
+        })
     }
 
     pub async fn encode(self) -> Result<Bytes, NetworkError> {
