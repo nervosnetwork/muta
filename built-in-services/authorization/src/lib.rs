7f6c5872c048201420b1df9d10b64131caf60fe7
KaoImin <kaoimin@qq.com>
2020-07-06 17:53:44 +0800

    refactor: change verify map to array

--- built-in-services/authorization/src/lib.rs
+++ built-in-services/authorization/src/lib.rs
@@ -1,58 +1,62 @@
 mod types;
 
 use binding_macro::{cycles, genesis, service};
-use protocol::traits::{ExecutorParams, ServiceResponse, ServiceSDK, StoreMap};
+use protocol::traits::{ExecutorParams, ServiceResponse, ServiceSDK, StoreArray};
 use protocol::types::{Address, ServiceContext};
 
 use crate::types::{
     AddVerifiedItemPayload, InitGenesisPayload, RemoveVerifiedItemPayload, SetAdminPayload,
+    VerifiedItem,
 };
 
 const AUTHORIZATION_ADMIN_KEY: &str = "authotization_admin";
-const MULTI_SIG_SERVICE: &str = "multi_signature";
-const MULTI_SIG_METHOD: &str = "verify_signature";
+
+lazy_static::lazy_static! {
+    static ref VERIFY_SIG: VerifiedItem = VerifiedItem {
+        service_name: String::from("multi_signature"),
+        method_name:  String::from("verify_signature"),
+    };
+}
 
 pub struct AuthorizationService<SDK> {
-    sdk:          SDK,
-    verified_map: Box<dyn StoreMap<String, String>>,
+    sdk:           SDK,
+    verified_list: Box<dyn StoreArray<VerifiedItem>>,
 }
 
 #[service]
 impl<SDK: ServiceSDK> AuthorizationService<SDK> {
     pub fn new(mut sdk: SDK) -> Self {
-        let mut verified_map: Box<dyn StoreMap<String, String>> =
-            sdk.alloc_or_recover_map("authotization");
+        let mut verified_list: Box<dyn StoreArray<VerifiedItem>> =
+            sdk.alloc_or_recover_array("authotization");
 
-        verified_map.insert(MULTI_SIG_SERVICE.to_owned(), MULTI_SIG_METHOD.to_owned());
+        if verified_list.is_empty() {
+            verified_list.push(VERIFY_SIG.clone());
+        }
 
-        Self { sdk, verified_map }
+        Self { sdk, verified_list }
     }
 
     #[genesis]
     fn init_genesis(&mut self, payload: InitGenesisPayload) {
-        let service_names = payload.register_service_names;
-        let function_names = payload.verified_method_names;
-        assert!(service_names.len() == function_names.len());
-
         self.sdk
             .set_value(AUTHORIZATION_ADMIN_KEY.to_string(), payload.admin);
 
-        for item in service_names.into_iter().zip(function_names.into_iter()) {
-            self.verified_map.insert(item.0, item.1);
+        for item in payload.verified_items.into_iter() {
+            self.verified_list.push(item);
         }
     }
 
     #[cycles(210_00)]
     #[read]
     fn check_authorization(&self, ctx: ServiceContext, payload: String) -> ServiceResponse<()> {
-        for (service_name, method_name) in self.verified_map.iter() {
-            let resp = self._do_verify(&ctx, &service_name, &method_name, &payload);
+        for (_idx, item) in self.verified_list.iter() {
+            let resp = self._do_verify(&ctx, &item.service_name, &item.method_name, &payload);
             if resp.is_error() {
                 return ServiceResponse::<()>::from_error(
                     102,
                     format!(
                         "verify transaction {:?} error {:?}",
-                        method_name, resp.error_message
+                        item.method_name, resp.error_message
                     ),
                 );
             }
@@ -72,8 +76,12 @@ impl<SDK: ServiceSDK> AuthorizationService<SDK> {
             return ServiceResponse::<()>::from_error(103, "Invalid caller".to_owned());
         }
 
-        self.verified_map
-            .insert(payload.service_name, payload.method_name);
+        let new_item = VerifiedItem::from(payload);
+        if self._check_exist(&new_item).is_some() {
+            return ServiceResponse::<()>::from_error(105, "Verified item exit".to_owned());
+        }
+
+        self.verified_list.push(new_item);
         ServiceResponse::from_succeed(())
     }
 
@@ -88,8 +96,21 @@ impl<SDK: ServiceSDK> AuthorizationService<SDK> {
             return ServiceResponse::<()>::from_error(103, "Invalid caller".to_owned());
         }
 
-        self.verified_map.remove(&payload.service_name);
-        ServiceResponse::from_succeed(())
+        let to_be_removed_item = VerifiedItem::from(payload);
+
+        if to_be_removed_item == VERIFY_SIG.clone() {
+            return ServiceResponse::<()>::from_error(
+                105,
+                "Can not remove verify signature".to_owned(),
+            );
+        }
+
+        if let Some(index) = self._check_exist(&to_be_removed_item) {
+            self.verified_list.remove(index);
+            ServiceResponse::from_succeed(())
+        } else {
+            return ServiceResponse::<()>::from_error(104, "Can not find item".to_owned());
+        }
     }
 
     #[cycles(210_00)]
@@ -123,5 +144,21 @@ impl<SDK: ServiceSDK> AuthorizationService<SDK> {
             .expect("must have an admin");
 
         ctx.get_caller() == admin
+    }
+
+    fn _check_exist(&self, verified_item: &VerifiedItem) -> Option<u64> {
+        let mut res = u64::MAX;
+        for (idx, item) in self.verified_list.iter() {
+            if &item == verified_item {
+                res = idx as u64;
+                break;
+            }
+        }
+
+        if res == u64::MAX {
+            None
+        } else {
+            Some(res)
+        }
     }
 }
