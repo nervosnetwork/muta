78ac5dee0e60e67e4d91520a5a1135d32513b536
zero.qn <zero.qn@outlook.com>
2020-07-14 08:34:02 +0800

    fix: invalid public key

--- core/network/src/outbound/gossip.rs
+++ core/network/src/outbound/gossip.rs
@@ -6,7 +6,7 @@ use protocol::{
 use tentacle::service::TargetSession;
 
 use crate::{
-    common::peer_id_from_pubkey_bytes,
+    common::peer_id_from_bytes,
     endpoint::Endpoint,
     error::NetworkError,
     message::{Headers, NetworkMessage},
@@ -64,13 +64,13 @@ where
     async fn multisend(
         &self,
         _ctx: Context,
-        pub_keys: Vec<Bytes>,
+        peer_ids: Vec<Bytes>,
         msg: Bytes,
         pri: Priority,
     ) -> Result<(), NetworkError> {
-        let peers = pub_keys
+        let peers = peer_ids
             .into_iter()
-            .map(peer_id_from_pubkey_bytes)
+            .map(peer_id_from_bytes)
             .collect::<Result<Vec<_>, _>>()?;
 
         self.sender.multisend(peers, msg, pri).await
