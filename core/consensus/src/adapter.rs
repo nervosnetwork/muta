3214367b0dda8a30a83b3bb027d96cb687954ae7
yejiayu <yejiayu.fe@gmail.com>
2020-07-27 17:43:44 +0800

    fix lint

--- core/consensus/src/adapter.rs
+++ core/consensus/src/adapter.rs
@@ -577,8 +577,8 @@ where
         let timestamp = block.header.timestamp;
         let current_timestamp = time_now();
         let consensus_interval = previous_metadata.interval;
-        
-        // The last block timestamp should be less than proposal timestamp.
+
+        // The previous block timestamp should be less than proposal timestamp.
         if previous_block.header.timestamp > timestamp {
             return Err(ConsensusError::InvalidTimestamp.into());
         }
