97708ac385be2243344d700a0d7c928f18fd51b3
Jiayu Ye <yejiayu.fe@gmail.com>
2020-07-29 10:20:40 +0800

    fix(mempool): Ensure that there are no duplicate transactions in the order transaction (#379)
    * fix(mempool): Ensure that there are no duplicate transactions in the order transaction
    
    * fix lint
    
    * fix lint

--- core/mempool/src/lib.rs
+++ core/mempool/src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(async_closure, test)]
-#![allow(clippy::suspicious_else_formatting)]
+#![allow(clippy::suspicious_else_formatting, clippy::mutable_key_type)]
 
 mod adapter;
 mod context;
@@ -15,6 +15,7 @@ pub use adapter::message::{
 pub use adapter::DefaultMemPoolAdapter;
 pub use adapter::{DEFAULT_BROADCAST_TXS_INTERVAL, DEFAULT_BROADCAST_TXS_SIZE};
 
+use std::collections::HashSet;
 use std::error::Error;
 use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
@@ -308,6 +309,8 @@ where
         height: Option<u64>,
         order_tx_hashes: Vec<Hash>,
     ) -> ProtocolResult<()> {
+        check_dup_order_hashes(&order_tx_hashes)?;
+
         let unknown_hashes = self.show_unknown_txs(order_tx_hashes).await;
         if !unknown_hashes.is_empty() {
             let unknown_len = unknown_hashes.len();
@@ -369,6 +372,20 @@ where
     }
 }
 
+fn check_dup_order_hashes(order_tx_hashes: &[Hash]) -> ProtocolResult<()> {
+    let mut dup_set = HashSet::with_capacity(order_tx_hashes.len());
+
+    for hash in order_tx_hashes.iter() {
+        if dup_set.contains(hash) {
+            return Err(MemPoolError::EnsureDup { hash: hash.clone() }.into());
+        }
+
+        dup_set.insert(hash.clone());
+    }
+
+    Ok(())
+}
+
 pub enum TxType {
     NewTx,
     ProposeTx,
@@ -411,6 +428,12 @@ pub enum MemPoolError {
 
     #[display(fmt = "Pull txs, require: {}, response: {}", require, response)]
     EnsureBreak { require: usize, response: usize },
+
+    #[display(
+        fmt = "There is duplication in order transactions. duplication tx_hash {:?}",
+        hash
+    )]
+    EnsureDup { hash: Hash },
 
     #[display(fmt = "Fetch full txs, require: {}, response: {}", require, response)]
     MisMatch { require: usize, response: usize },
