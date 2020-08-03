9f1a877e635eb6ae4216c2b0321b2990b8bf0357
KaoImin <kaoimin@qq.com>
2020-08-03 11:12:43 +0800

    feat: add random leader feature

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,5 +1,25 @@
 #![feature(vec_remove_item)]
 #![feature(test)]
 
+#[macro_export]
+macro_rules! impl_interface {
+    ($self: expr, $method: ident, $ctx: expr) => {{
+        let res = $self.$method($ctx.clone());
+        if res.is_error() {
+            Err(ServiceResponse::from_error(res.code, res.error_message))
+        } else {
+            Ok(res.succeed_data)
+        }
+    }};
+    ($self: expr, $method: ident, $ctx: expr, $payload: expr) => {{
+        let res = $self.$method($ctx.clone(), $payload);
+        if res.is_error() {
+            Err(ServiceResponse::from_error(res.code, res.error_message))
+        } else {
+            Ok(res.succeed_data)
+        }
+    }};
+}
+
 pub mod binding;
 pub mod executor;
