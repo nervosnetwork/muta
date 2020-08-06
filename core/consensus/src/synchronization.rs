d57518a520e30fb6c326d403242217e96158dff0
yejiayu <yejiayu.fe@gmail.com>
2020-08-06 15:05:46 +0800

    fix lint

--- core/consensus/src/synchronization.rs
+++ core/consensus/src/synchronization.rs
@@ -153,7 +153,7 @@ impl<Adapter: SynchronizationAdapter> OverlordSynchronization<Adapter> {
         } else {
             remote_height + ONCE_SYNC_BLOCK_LIMIT
         };
-    
+
         let mut current_consented_height = current_height;
 
         while current_consented_height < remote_height {
