4fe38e18403ee05e4e450cac0e175028038f5fba
KaoImin <kaoimin@qq.com>
2020-08-06 19:42:57 +0800

    perf(state): add state cache for trieDB

--- framework/src/binding/state/trie_db.rs
+++ framework/src/binding/state/trie_db.rs
@@ -1,17 +1,25 @@
+use std::collections::HashMap;
 use std::path::Path;
 use std::sync::Arc;
 use std::time::Instant;
 
 use bytes::Bytes;
 use derive_more::{Display, From};
+use parking_lot::RwLock;
+use rand::{rngs::SmallRng, Rng, SeedableRng};
 use rocksdb::{Options, WriteBatch, DB};
 
 use common_apm::metrics::storage::{on_storage_get_state, on_storage_put_state};
 use protocol::{ProtocolError, ProtocolErrorKind, ProtocolResult};
 
+const RAND_SEED: u64 = 49999;
+const MAP_SIZE: usize = 2000;
+
 pub struct RocksTrieDB {
     light: bool,
     db:    Arc<DB>,
+
+    cache: RwLock<HashMap<Vec<u8>, Vec<u8>>>,
 }
 
 impl RocksTrieDB {
@@ -26,7 +34,20 @@ impl RocksTrieDB {
         Ok(RocksTrieDB {
             light,
             db: Arc::new(db),
+            cache: RwLock::new(HashMap::new()),
         })
+    }
+
+    fn inner_get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, RocksTrieDBError> {
+        let res = {
+            let cache = self.cache.read();
+            cache.get(key).cloned()
+        };
+
+        if res.is_none() {
+            return Ok(self.db.get(key).map_err(to_store_err)?);
+        }
+        Ok(res)
     }
 }
 
@@ -36,19 +57,33 @@ impl cita_trie::DB for RocksTrieDB {
     fn get(&self, key: &[u8]) -> Result<Option<Vec<u8>>, Self::Error> {
         let inst = Instant::now();
 
-        let res = self.db.get(key).map_err(to_store_err)?.map(|v| v.to_vec());
+        let res = self.inner_get(key)?;
 
         on_storage_get_state(inst.elapsed(), 1i64);
         Ok(res)
     }
 
     fn contains(&self, key: &[u8]) -> Result<bool, Self::Error> {
-        Ok(self.db.get(key).map_err(to_store_err)?.is_some())
+        let res = {
+            let cache = self.cache.read();
+            cache.contains_key(key)
+        };
+
+        if res {
+            Ok(true)
+        } else {
+            Ok(self.db.get(key).map_err(to_store_err)?.is_some())
+        }
     }
 
     fn insert(&self, key: Vec<u8>, value: Vec<u8>) -> Result<(), Self::Error> {
         let inst = Instant::now();
         let size = key.len() + value.len();
+
+        {
+            let mut cache = self.cache.write();
+            cache.insert(key.clone(), value.clone());
+        }
 
         self.db
             .put(Bytes::from(key), Bytes::from(value))
@@ -67,13 +102,15 @@ impl cita_trie::DB for RocksTrieDB {
 
         let mut total_size = 0;
         let mut batch = WriteBatch::default();
-        for i in 0..keys.len() {
-            let key = &keys[i];
-            let value = &values[i];
 
-            total_size += key.len();
-            total_size += value.len();
-            batch.put(key, value);
+        {
+            let mut cache = self.cache.write();
+            for (key, val) in keys.iter().zip(values.iter()) {
+                total_size += key.len();
+                total_size += val.len();
+                batch.put(key, val);
+                cache.insert(key.clone(), val.clone());
+            }
         }
 
         self.db.write(batch).map_err(to_store_err)?;
@@ -84,6 +121,10 @@ impl cita_trie::DB for RocksTrieDB {
 
     fn remove(&self, key: &[u8]) -> Result<(), Self::Error> {
         if self.light {
+            {
+                let mut cache = self.cache.write();
+                cache.remove(key);
+            }
             self.db.delete(key).map_err(to_store_err)?;
         }
         Ok(())
@@ -92,19 +133,50 @@ impl cita_trie::DB for RocksTrieDB {
     fn remove_batch(&self, keys: &[Vec<u8>]) -> Result<(), Self::Error> {
         if self.light {
             let mut batch = WriteBatch::default();
-            for key in keys {
-                batch.delete(key);
+            {
+                let mut cache = self.cache.write();
+                for key in keys {
+                    cache.remove(key);
+                    batch.delete(key);
+                }
             }
 
             self.db.write(batch).map_err(to_store_err)?;
         }
-
         Ok(())
     }
 
     fn flush(&self) -> Result<(), Self::Error> {
+        let mut cache = self.cache.write();
+        let len = cache.len();
+
+        if len <= MAP_SIZE {
+            return Ok(());
+        }
+
+        let keys = cache.keys().collect::<Vec<_>>();
+        let remove_list = rand_remove_list(keys, len - MAP_SIZE);
+
+        for item in remove_list.iter() {
+            cache.remove(item);
+        }
         Ok(())
     }
+}
+
+fn rand_remove_list<T: Clone>(keys: Vec<&T>, num: usize) -> Vec<T> {
+    let mut len = keys.len() - 1;
+    let mut idx_list = (0..len).map(|i| i).collect::<Vec<_>>();
+    let mut rng = SmallRng::seed_from_u64(RAND_SEED);
+    let mut ret = Vec::new();
+
+    for _ in 0..num {
+        let tmp = rng.gen_range(0, len);
+        let idx = idx_list.remove(tmp);
+        ret.push(keys[idx].to_owned());
+        len -= 1;
+    }
+    ret
 }
 
 #[derive(Debug, Display, From)]
@@ -133,4 +205,22 @@ impl From<RocksTrieDBError> for ProtocolError {
 fn to_store_err(e: rocksdb::Error) -> RocksTrieDBError {
     log::error!("[framework] trie db {:?}", e);
     RocksTrieDBError::Store
+}
+
+#[cfg(test)]
+mod benchmark {
+    extern crate test;
+    use test::Bencher;
+
+    use super::*;
+
+    #[bench]
+    fn bench_insert(b: &mut Bencher) {
+        b.iter(|| {
+            let mut rng = SmallRng::seed_from_u64(49999);
+            for _ in 0..10000 {
+                rng.gen_range(10, 1000000);
+            }
+        })
+    }
 }
